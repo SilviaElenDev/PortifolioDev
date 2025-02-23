d5baef0f56b04fa1bb35ff1fc7e53fd04abd29ed
Silvia <silviaelen.developer@gmail.com>
2025-02-23 15:43:13 -0300

    Portifólio 100% funcional para desktop

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ menu.addEventListener('click',() => {
         listNav.style.display = 'none'
     }
 })
+
+
+let sliderContainer = document.querySelector('.slider-container')
+let next = document.querySelector('.next')
+let back = document.querySelector('.back')
+
+
+
+let position = 0; 
+
+
+back.addEventListener('click', () => {
+    if (position >= -41){
+        position -= 41;
+        sliderContainer.style.transition = 'transform 1s'
+        sliderContainer.style.transform = `translateX(${position}vw)`
+    }else{
+        return;
+    }
+    
+})
+
+next.addEventListener('click', () => {
+    if(position <= +41){
+        position += 41;
+        sliderContainer.style.transition = 'transform 1s'
+        sliderContainer.style.transform = `translateX(${position}vw)`
+    }else{
+        return;
+    }
+    
+})
